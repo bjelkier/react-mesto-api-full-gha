b6c5d4ee4a4c666cc3ed8cc0eade366f1a312669
Albina Makarova <albinamakarova@rambler.ru>
2023-09-09 00:49:03 +0300

    change db connection address

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use((err, req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
