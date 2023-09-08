1b328f12e5e9122a8ab2c5b3dbb4c81e5781eccd
Albina Makarova <albinamakarova@rambler.ru>
2023-09-09 01:10:46 +0300

    add builded front

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -86,7 +86,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: 'http://localhost:9000',
+  baseUrl: 'http://localhost:6000',
   headers: {
     authorization: '4f377772-e7bc-4af1-a1d5-19f46cf40a60',
     'Content-Type': 'application/json'
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,6 +1,6 @@
 class Auth {
   constructor() {
-    this._baseUrl = 'http://localhost:9000';
+    this._baseUrl = 'http://localhost:6000';
     this._headers = {
       'Content-Type': 'application/json'
     }
