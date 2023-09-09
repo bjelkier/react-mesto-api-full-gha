a44fc4b8f18dac759aa9ad48e60a35fe7fb710d6
Albina Makarova <albinamakarova@rambler.ru>
2023-09-10 01:03:48 +0300

    add env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const WrongData = require('../errors/WrongData');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
@@ -8,9 +11,9 @@ module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, 'iam-extra-tired');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new WrongData('Требуется авторизация'));
+    next(new WrongData('Необходима авторизация'));
   }
   req.user = payload;
   next();
